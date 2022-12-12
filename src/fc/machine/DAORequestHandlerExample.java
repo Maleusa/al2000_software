05dc810ebd5646d88fa2ff79ea31c2383c856a6e
Maleusa <yazid.cheriti@gmail.comgit config --global user.email yazid.cheriti@gmail.com>
2022-12-12 23:42:15 +0100

    movie_exemple

--- src/fc/machine/DAORequestHandlerExample.java
+++ src/fc/machine/DAORequestHandlerExample.java
@@ -3,8 +3,12 @@ package fc.machine;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import fc.searchengine.GenreTag;
 import fc.searchengine.Tag;
+import fc.user.Guest;
+import fc.user.Subscriber;
 import fc.user.User;
+import fc.user.UserHistoric;
 import fc.constants.MovieState;
 public class DAORequestHandlerExample implements DAORequestHandler{
 	
@@ -36,16 +40,34 @@ public class DAORequestHandlerExample implements DAORequestHandler{
 
 	@Override
 	public User getUser(int userID) {
-		
-		return null;
+		User thibaud=new Subscriber(userID);
+		thibaud.setAdress("12 rue des lapins");
+		thibaud.setHistoric(new UserHistoric());
+		thibaud.setName("Thibaud");
+		thibaud.setPassword("1234");
+		thibaud.setRestrictedGenre(new ArrayList<GenreTag>());
+		return thibaud;
 	}
 
 	@Override
 	public ArrayList<QRCode> getRequestedDigitalMovies(HashMap<String, ArrayList<Tag>> requests) {
+		QRCode thebatman = new QRCode();
+		QRCode badboys = new QRCode();
+		QRCode therock = new QRCode();
+		
+		
 		ArrayList<QRCode> qrCodes = new ArrayList<QRCode>();
 		ArrayList<Movie> movies = movieCreator();
+		qrCodes.add(thebatman);
+		qrCodes.add(badboys);
+		qrCodes.add(therock);
 		
-		return null;
+		for(int i=0; i<3; i++) {
+			
+			qrCodes.get(i).setMovie(movies.get(i));
+			
+		}
+		return qrCodes;
 	}
 
 
@@ -55,22 +77,23 @@ public class DAORequestHandlerExample implements DAORequestHandler{
 		Movie moviethebatman = new Movie();
 		Movie moviebadboys = new Movie();
 		Movie movietherock = new Movie();
+		Movie moviegattaca =new Movie();
 		
 		moviethebatman.setTitle("The batman");
 		moviethebatman.setDirectors("Matt Reeves");
-		moviethebatman.setActors("Robert Pattinson;Zoë Kravitz;Paul Dano");
+		moviethebatman.setActors("Robert Pattinson;Zoï¿½ Kravitz;Paul Dano");
 		moviethebatman.setDescription("The batman, il bien et dure trois heures.");
-		
+		moviethebatman.setUrl("https://m.media-amazon.com/images/I/81DGyn3r62L.jpg");
 		moviebadboys.setTitle("Bad boys");
 		moviebadboys.setDirectors("Micheal Bay");
 		moviebadboys.setActors("Will Smith;Martin Lawrence;Joe Pantaliano");
 		moviebadboys.setDescription("Bad boys, bad boys what you gonna do when ...");
-		
+		moviebadboys.setUrl("https://www.ecranlarge.com/media/cache/1600x1200/uploads/image/001/121/l3daia6meglcxxdoun8mqgfms7w-846.jpg");
 		movietherock.setTitle("The Rock");
 		movietherock.setDirectors("Micheal Bay");
 		movietherock.setActors("Sean Connery;Nicolas Cage;Ed Harris");
 		movietherock.setDescription("The Rock, le meilleur film de Micheal Bay, une suite de James Bond ??");
-		
+		movietherock.setUrl("https://upload.wikimedia.org/wikipedia/en/8/82/The_Rock_%28movie%29.jpg");
 		/*
 		 * AJOUTER URL
 		 */
@@ -84,8 +107,13 @@ public class DAORequestHandlerExample implements DAORequestHandler{
 
 	@Override
 	public User getUserFromCB(int numCB) {
-		// TODO Auto-generated method stub
-		return null;
+		User thibaud=new Guest(numCB);
+		thibaud.setAdress("");
+		thibaud.setHistoric(new UserHistoric());
+		thibaud.setName("Guest");
+		thibaud.setPassword("");
+		thibaud.setRestrictedGenre(new ArrayList<GenreTag>());
+		return thibaud;
 	}
 
 }
